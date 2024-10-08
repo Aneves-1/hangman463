e09ce40c8488446748e5fc945e7a113e4f0a442f
Andre Neves <aneves_1@hotmail.com>
2024-10-08 16:42:05 +0100

    fix auto win or loose. added docstrings

--- milestone_5.py
+++ milestone_5.py
@@ -1,6 +1,40 @@
 import random
 
 class Hangman:
+    '''
+    A Hangman Game that asks the user for a letter and checks if it is in the word.
+    It starts with a default number of lives and a random word from the word_list.
+
+    
+    Parameters:
+    ----------
+    word_list: list
+        List of words to be used in the game
+    num_lives: int
+        Number of lives the player has
+    
+    Attributes:
+    ----------
+    word: str
+        The word to be guessed picked randomly from the word_list
+    word_guessed: list
+        A list of the letters of the word, with '_' for each letter not yet guessed
+        For example, if the word is 'apple', the word_guessed list would be ['_', '_', '_', '_', '_']
+        If the player guesses 'a', the list would be ['a', '_', '_', '_', '_']
+    num_letters: int
+        The number of UNIQUE letters in the word that have not been guessed yet
+    num_lives: int
+        The number of lives the player has
+    list_letters: list
+        A list of the letters that have already been tried
+
+    Methods:
+    -------
+    check_letter(letter)
+        Checks if the letter is in the word.
+    ask_letter()
+        Asks the user for a letter.
+    '''
     def __init__(self, word_list, num_lives = 5):
         self.word = random.choice(word_list)
         self.word_guessed = len(self.word) * ["_"]
@@ -8,12 +42,23 @@ class Hangman:
         self.num_letters = len(set(self.word))
         self.list_of_guesses = []
     def check_guess(self, guess):
+        '''
+        Checks if the letter is in the word.
+        If it is, it replaces the '_' in the word_guessed list with the letter.
+        If it is not, it reduces the number of lives by 1.
+
+        Parameters:
+        ----------
+        guess: str
+            The letter that was guessed to be checked
+
+        '''
         guess = str.lower(guess)
         word_lower = str.lower(self.word)
         guess_count = word_lower.count(guess)
         if guess in self.word:
             print(f"Good guess! {guess} is in the word.")
-            self.num_letters -= 1
+            
             if guess_count >= 1:   
                 guess_indices = []
                 guess_index = word_lower.index(guess)
@@ -25,18 +70,22 @@ class Hangman:
                     count += 1
                 for i in guess_indices:
                     self.word_guessed[i] = guess
-                            
+                self.num_letters -= 1            
         else:
             self.num_lives -= 1
             print(f"Sorry, {guess} is not in the word.")
             print(f"You have {self.num_lives} lives left.")
         self.list_of_guesses.append(guess)
-        print(f"Letters tried: {set(self.list_of_guesses)}")
         print(f"{self.word_guessed}")           
     def ask_for_input(self):
-    
+        '''
+        Asks the user for a letter and checks two things:
+        1. If the character is a single character
+        2. If the letter has already been tried
+        If it passes both checks, it calls the check_letter method.
+        '''
         while True:
-            guess = input("Enter your letter choice: ")            
+            guess = input("Enter your letter guess: ")            
             if len(guess) != 1 or guess.isalpha() == False:
                 print("Invalid letter. Please, enter a single alphabetical character.")
             elif guess in self.list_of_guesses:
@@ -44,6 +93,7 @@ class Hangman:
             elif len(guess) == 1 and guess.isalpha() and guess not in self.list_of_guesses:
                 self.check_guess(guess)
                 self.list_of_guesses.append(guess)
+                break
              
 def play_game(word_list):
     
